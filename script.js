bf2141b3abeee56e02e9ac5eac485c935614852e
charleswelausen <cwelausen@hotmail.com>
2024-02-22 12:18:26 -0300

    desafio decodificador de texto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let textoOriginal = document.querySelector('textarea');
-let textoCriptografado = document.getElementById('paragrafo');
+let textoCriptografado = document.getElementById('output');
 
-let paraBut = document.querySelector('#paragrafo-buton');
+let paraBut = document.querySelector('#output-buton');
 let imgText = document.querySelector('#imagem-texto');
 
 let textoCopiado = '';
@@ -10,7 +10,7 @@ function criptografar() {
     let texto = textoOriginal.value;
     let cripto = texto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
     
-    textoCopiado = document.getElementById('paragrafo').innerHTML = cripto;
+    textoCopiado = document.getElementById('output').innerHTML = cripto;
 
     paraBut.style.display="flex";
     imgText.style.display="none";
@@ -21,7 +21,7 @@ function descriptografar() {
     let texto = textoOriginal.value;
     let descripto = texto.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, "a").replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
     
-    textoCopiado = document.getElementById('paragrafo').innerHTML = descripto;
+    textoCopiado = document.getElementById('output').innerHTML = descripto;
 
     paraBut.style.display="flex";
     imgText.style.display="none";
